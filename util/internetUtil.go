3eee41c5464333874bf21a9fce5a6cc7c4d238c0
eatPorkAndSeePigRun <811302964@qq.com>
2018-08-24 00:58:05 +0800

    睡觉了

--- util/internetUtil.go
+++ util/internetUtil.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"net"
+	"bytes"
 )
 
 type InternetUtil struct {
@@ -22,12 +23,88 @@ func (iu InternetUtil) myMainThread() {
 }
 func handleClient(conn net.Conn) {
 	defer conn.Close()
-	// TODO: 接收解析传感器数据,并关闭
-	conn.Write([]byte("some smg"))
+	data, err := readData(conn)
+	checkError(err)
+	msg := decode(data)
+	// TODO: 如需发信号
+	conn.Write([]byte(msg))
+}
+
+func readData(conn net.Conn) ([]byte, error) {
+	var data, result []byte
+	for true {
+		_, err := conn.Read(data)
+		if err != nil {
+			return nil, err
+		}
+		for _, v := range data {
+			temp := []byte{v}
+			if !bytes.Equal(temp, []byte("\xAA")) {
+				result = append(result, v)
+			} else {
+				return result[2:], nil
+			}
+		}
+	}
+
+}
+
+func decode(data []byte) (string) {
+	err := decodeDataLength(data[0], len(data))
+	checkError(err)
+	err = decodeCommandIdentifier(data[1])
+	checkError(err)
+	return ""
+}
+
+func decodeDataLength(dataLength byte, dataRealLength int) (error) {
+	if int(dataLength) != (dataRealLength - 2) {
+		return &decodeError{"数据长度不对"}
+	}
+	return nil
+}
+
+func decodeCommandIdentifier(commandIdentifier byte) (error) {
+	//TODO: 添加命令标识符处理逻辑
+	if commandIdentifier == 0xF1 {
+		//基板：Read读取命令			传感器模块：发送数据命令
+	} else if commandIdentifier == 0xF2 {
+		//基板：Test测试命令			传感器模块：接收数据命令
+	} else if commandIdentifier == 0xF3 {
+		//基板：Test测试确认命令		传感器模块：发送一次数据命令
+	} else if commandIdentifier == 0xF4 {
+		//基板：查询传感器模块地址	传感器模块：发送传感器地址
+	}
+	return nil
+}
+
+func decodeSensorName(sensorName []byte) (error) {
+	//TODO
+	return nil
+}
+
+func decodeSensorModuleAddress() {
+
+}
+
+func decodeSensorData() {
+
+}
+
+func decodeCheckCode() {
+
 }
 
 func checkError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+type decodeError struct {
+	info string
+}
+
+func (e *decodeError) Error() string {
+	return e.info
 }
